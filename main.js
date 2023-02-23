1c5afc339eb3c5dcde4790b9d7e8bf515485648b
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-02-23 17:05:30 +0300

    add cicle for and if else

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - возвращаем пользователя к вопросам опять
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
 
 'use strict';
 
@@ -33,14 +23,22 @@ let personalMovieDB = {
   privat: false,
 }
 
-const a = prompt('The name of the last movie you watched?', ''),
-      b = prompt('What rating was given to the film?', ''),
-      c = prompt('The name of the last movie you watched?', ''),
-      d = prompt('What rating was given to the film?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('The name of the last movie you watched?', ''),
+          b = prompt('What rating was given to the film?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-// personalMovieDB['movies'] = {[nameLastFilms]: raitingFilms};
+    if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if(personalMovieDB.count < 10) {
+    alert('Watched few films');
+} else if(personalMovieDB.count > 9 && personalMovieDB.count < 31) {
+    alert('You are a classic spectator');
+} else alert('You a cinephile');
+
 
 console.log(personalMovieDB);
-console.log(personalMovieDB['movies']);
